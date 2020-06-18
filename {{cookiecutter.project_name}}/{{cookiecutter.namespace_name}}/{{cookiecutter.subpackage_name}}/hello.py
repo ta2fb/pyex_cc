8dd2e26c8e244f5dbea1e779c914152be282359c
Thomas Adam <tadam@biocorellc.com>
2020-06-18 09:04:57 -0400

    update hello.py documentation

--- {{cookiecutter.project_name}}/{{cookiecutter.namespace_name}}/{{cookiecutter.subpackage_name}}/hello.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.namespace_name}}/{{cookiecutter.subpackage_name}}/hello.py
@@ -1,10 +1,18 @@
 """Hello file"""
 
 
-def hello_world():
-    """Hello world documentation!
+def hello_world(text: str = 'hello world') -> str:
+    """Hello world documentation
 
-    :return: string
+    This project uses the Google Style Python Docstrings
+
+    https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+
+    Args:
+        text: Text to return
+
+    Returns:
+        String of text passed to the function
     """
 
-    return 'hello world'
+    return text
