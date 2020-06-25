7e241982a09eed16f9c7f198837516215dac6ed1
Thomas Adam <tadam@biocorellc.com>
2020-06-25 10:47:11 -0400

    add autoclass_content both docs config

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -58,3 +58,6 @@ html_theme = 'pyramid'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# ensure both class and init comments get included in the docs
+autoclass_content = 'both'
