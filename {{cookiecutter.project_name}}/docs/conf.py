61a2c79b269d97311646bd13a5202e3a8f651551
Thomas Adam <tadam@biocorellc.com>
2020-06-26 08:42:13 -0400

    use the readthedocs theme

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -33,6 +33,7 @@ extensions = [
         'sphinx.ext.autosummary',
         'sphinx.ext.todo',
         'sphinx.ext.viewcode',
+        'sphinx_rtd_theme',
         'sphinxcontrib.napoleon'
 ]
 
@@ -52,7 +53,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pyramid'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
